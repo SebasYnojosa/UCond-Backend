6f269ccfbc42ffe388e6f6ba245ada8ce687a4ac
gabrielmj23 <gabrielmj2312@gmail.com>
2023-11-21 11:47:39 -0400

    Crear router de usuarios y proteger con autenticación

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
 
-//ayuda a cargar las variables
-//de entorno correctamente ya que daba error en los get
+// Ayuda a cargar las variables
+// de entorno correctamente ya que daba error en los get
 import dotenv from "dotenv";
 dotenv.config();
 
-//rutas
+// Rutas
 import { authRouter } from "./routes/auth";
-import consultaUsuarioRouter from "./routes/consultaUsuario";
-import modificarUsuarioRouter from "./routes/modificarUsuario";
-import eliminarUsuarioRouter from "./routes/eliminarUsuario";
+import { usuariosRouter } from "./routes/usuarios";
+import { authProtected } from "../utils/auth";
+
 // Inicializar aplicación
 const app = express();
 app.use(express.json());
@@ -18,8 +18,6 @@ const PORT = Number(process.env.port) || 3000;
 // Registrar rutas
 app.get("/api", (_req, res) => res.send("Hello world!"));
 app.use("/api/auth", authRouter);
-app.use("/api/consultausuario", consultaUsuarioRouter);
-app.use("/api/modificarusuario", modificarUsuarioRouter);
-app.use("/api/eliminarusuario", eliminarUsuarioRouter);
+app.use("/api/usuarios", authProtected, usuariosRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
