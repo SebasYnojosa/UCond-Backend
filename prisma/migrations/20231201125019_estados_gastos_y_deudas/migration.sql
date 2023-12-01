b37aeb517981a016aaf9be3649466896a21aa308
gabrielmj23 <gabrielmj2312@gmail.com>
2023-12-01 08:50:44 -0400

    Estados de deudas y gastos

---
+++ prisma/migrations/20231201125019_estados_gastos_y_deudas/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Deuda" ADD COLUMN     "activa" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "Gasto" ADD COLUMN     "activo" BOOLEAN NOT NULL DEFAULT true;
