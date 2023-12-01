f55005eca85e66638379a765f666c6702d404622
gabrielmj23 <gabrielmj2312@gmail.com>
2023-12-01 00:07:20 -0400

    Juntar gastos con main

--- src/routes/gastos.ts
+++ src/routes/gastos.ts
@@ -6,52 +6,47 @@ const gastoSchema = z.object({
     id_condominio: z
         .number()
         .min(1, "Se debe especificar la id del condominio"),
-    monto: z
-        .number(),
+    monto: z.number(),
     concepto: z
         .string()
         .trim()
         .min(1, "El concepto del gasto no puede estar vacio")
         .max(255),
-    fecha_limite: z
-        .coerce
-        .date(),
-    tipo: z
-        .string()
-        .trim()
-        .min(1, "El tipo no puede estar vacio")
-        .max(255)
+    fecha_limite: z.coerce.date(),
+    tipo: z.string().trim().min(1, "El tipo no puede estar vacio").max(255),
 });
 
 export const gastosRouter = Router();
 const prisma = new PrismaClient();
 
-gastosRouter.post('/', async (req, res) => {
+gastosRouter.post("/", async (req, res) => {
     try {
         // Convierte el monto de string a numero
         req.body.monto = Number(req.body.monto);
 
         // Verifica que el id del condominio exista
         const condo = await prisma.condominio.findUnique({
-            where: { id: req.body.id_condominio }
+            where: { id: req.body.id_condominio },
         });
 
-        if (!condo) return res.status(404).json({ error: "El condominio no existe" });
+        if (!condo)
+            return res.status(404).json({ error: "El condominio no existe" });
 
-        const gasto = gastoSchema.parse({ ...req.body })
+        const gasto = gastoSchema.parse({ ...req.body });
 
         // Crea el gasto
         const nuevoGasto = await prisma.gasto.create({
             data: {
-                ...gasto
-            }
-        })
-        res.json({gasto: nuevoGasto});
+                ...gasto,
+            },
+        });
+        res.json({ gasto: nuevoGasto });
     } catch (error) {
-        if (error instanceof z.ZodError) 
-            return res.status(400).json({error: "Datos invalidos", mensaje: error.issues});
+        if (error instanceof z.ZodError)
+            return res
+                .status(400)
+                .json({ error: "Datos invalidos", mensaje: error.issues });
         res.status(500).json(error);
         console.log(error);
     }
-})
-
+});
