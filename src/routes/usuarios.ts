e9c9bc207e59431dd19e140321adb8dcced4e5d5
gabrielmj23 <gabrielmj2312@gmail.com>
2023-12-06 11:42:02 -0400

    Ver condominios de usuario

--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -100,4 +100,81 @@ usuariosRouter.put("/:id", async (req, res) => {
     }
 });
 
+/**
+ * GET /api/usuarios/:userId/condominios
+ * Busca los condominios de un usuario por su ID
+ */
+usuariosRouter.get("/:userId/condominios", async (req, res) => {
+    try {
+        const userId = parseInt(req.params.userId); // Obtener el ID de los parámetros de la URL
+        const viviendas_usuario = await prisma.vivienda.findMany({
+            where: { id_propietario: userId },
+            include: { condominio: true },
+        });
+        // Filtrar todos los condominios únicos de las viviendas del usuario
+        const condominios = viviendas_usuario
+            .map((vivienda) => vivienda.condominio)
+            .filter(
+                (condominio, index, self) =>
+                    self.findIndex((c) => c.id === condominio.id) === index,
+            );
+        res.json({ condominios });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error al buscar los condominios del usuario",
+        });
+    }
+});
+
+/**
+ * GET /api/usuarios/:userId/deudas?idCondominio=<idCondominio>
+ * Busca las deudas de un usuario por su ID
+ */
+usuariosRouter.get("/:userId/deudas", async (req, res) => {
+    try {
+        // Obtener el ID de los parámetros de la URL
+        const userId = parseInt(req.params.userId);
+        // Buscar usuario por ID en la base de datos
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+            include: { deudas: true },
+        });
+        if (!user) {
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+        // Trabajo extra si se especifica la id del condominio
+        if (req.query.idCondominio) {
+            const idCondominio = parseInt(req.query.idCondominio as string);
+            // Buscar condominio por ID en la base de datos
+            const condominio = await prisma.condominio.findUnique({
+                where: { id: idCondominio },
+                include: { gastos: true },
+            });
+            if (!condominio) {
+                return res
+                    .status(404)
+                    .json({ error: "Condominio no encontrado" });
+            }
+            // Devolver deudas activas que pertenezcan al condominio
+            return res.json({
+                deudas: user.deudas.filter(
+                    (deuda) =>
+                        deuda.activa &&
+                        condominio.gastos.some(
+                            (gasto) => gasto.id === deuda.id_gasto,
+                        ),
+                ),
+            });
+        }
+        // Devolver las deudas activas del usuario
+        res.json({ deudas: user.deudas.filter((deuda) => deuda.activa) });
+    } catch (error) {
+        // Manejo de errores
+        console.log(error);
+        res.status(500).json({
+            error,
+        });
+    }
+});
+
 export { usuariosRouter };
